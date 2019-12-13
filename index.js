2f5ccac9b30786627c0ca7554eab9ae5e6422169
Melanie Mendoza <melanie.mendoza@gmail.com>
2019-12-12 22:25:55 -0500

    mvp

--- index.js
+++ index.js
@@ -39,8 +39,25 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+     this.name = name;
+     this.age = age;
+     this.stomach =[];
+}
 
+Person.prototype.eat = function(food) {
+  if (this.stomach.length >= 10) {
+    return;
+  }
+  this.stomach.push(food)
+}
+
+Person.prototype.poop = function() {
+  this.stomach.length = 0;
+}
+
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`
 }
 
 /*
@@ -57,8 +74,28 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+}
 
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
+}
+
+Car.prototype.drive = function(distance) {
+  const fuel = this.tank * this.milesPerGallon;
+  if (fuel < distance) {
+    this.tank = 0;
+    this.odometer += fuel;
+    return `I ran out of fuel at ${this.odometer} miles!`
+  }
+
+  const gallons = distance / this.milesPerGallon;
+  this.odometer += distance;
+  this.tank -= gallons;
 }
 
 /*
@@ -68,18 +105,24 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age)
+  this.favoriteToy = favoriteToy
+}
 
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Principle 1: Window/Global Object Binding. When in the global scope, the value of “this” will be the window/console Object;
+  2. Principle 2: Implicit Binding. Whenever a preceding dot calls a function, the object before the dot is this.
+  3. Principle 3: New binding. Whenever we use a constructor function, this refers to the specific instance of the object that is created and returned by the constructor function.
+  4. Principle 4: Explicit binding. Whenever we use JavaScript’s call or apply method, this is explicitly defined.
 */
 
 
